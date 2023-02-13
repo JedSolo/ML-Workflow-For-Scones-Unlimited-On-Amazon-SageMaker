9f1b69272b7fc291ab91fd2c137b6d9e94faa8df
Solomon <solomonsarfo045@gmail.com>
2023-02-13 15:57:51 +0000

    working phase

---
+++ lambda2.py
@@ -0,0 +1,33 @@
+import json
+import sagemaker
+import base64
+from sagemaker.serializers import IdentitySerializer
+
+# Fill this in with the name of your deployed model
+ENDPOINT = "image-classification-2023-02-13-13-59-38-382"
+
+def lambda_handler(event, context):
+
+    # # Decode the image data
+    image = base64.b64decode(event['image_data'])
+
+    # Instantiate a Predictor
+    predictor = sagemaker.predictor.Predictor(ENDPOINT, sagemaker_session=sagemaker.Session())
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+    
+    # Make a prediction:
+    inferences = predictor.predict(image)
+    
+    # We return the data back to the Step Function    
+    event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": event['image_data'],
+            "s3_bucket": event['s3_bucket'],
+            "s3_key": event['s3_key'],
+            "inferences": event["inferences"]
+        }
+    }
